3b19eb5854a43d40985a89522e9fea2a3ac2c4e6
Tharindu De Zoysa <tharindu.2015188@iit.ac.lk>
2025-10-22 16:25:14 +0530

    update

--- src/routes/metadata.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { fetchMetadata } from '../controllers/metadata.controller';
-
-const router = Router();
-// POST /api/metadata  { url }
-router.post('/', fetchMetadata);
-
-export default router;
--- src/utils/password.ts
+++
@@ -1,9 +0,0 @@
-import * as bcrypt from 'bcryptjs'; 
-
-export async function hashPassword(plain: string) {
-  const salt = await bcrypt.genSalt(10);
-  return bcrypt.hash(plain, salt);
-}
-export async function verifyPassword(plain: string, hash: string) {
-  return bcrypt.compare(plain, hash);
-}
